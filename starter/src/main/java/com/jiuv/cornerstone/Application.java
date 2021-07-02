49b710e63b4e6cb4d145f41de43a9fd79364659c
jianghs430 <jianghs430@zto.com>
2021-07-02 16:07:36 +0800

    change

--- starter/src/main/java/com/jiuv/cornerstone/Application.java
+++ starter/src/main/java/com/jiuv/cornerstone/Application.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version: 1.0
  */
 @SpringBootApplication(scanBasePackages = {"com.jiuv.cornerstone"})
-//@MapperScan("com.jiuv.cornerstone.gatewayimpl")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
